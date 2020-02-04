6ae6af24f34b65be93ce815f98043bdc6cafc3ca
Enrique David Jose Incio Chapilliquen <enrique.incio@gmail.com>
2020-02-03 20:19:41 -0500

    Consumiendo API de SUNAT para obtener RUC

---
+++ ConsultaRUCApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace ConsultaRUCApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = "https://api.sunat.cloud/ruc/10409047594";
+
+            HttpWebRequest request = WebRequest.Create(endpoint) as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            RUC ruc = JsonConvert.DeserializeObject<RUC>(json);
+
+            Console.WriteLine($"Razon Social: {ruc.ruc}");
+            Console.WriteLine($"RUC: {ruc.razon_social}");
+            Console.WriteLine($"Nombre Comercial: {ruc.nombre_comercial}");
+
+            //Console.WriteLine(json);
+            Console.Read();
+        }
+    }
+}
---
+++ ConsultaRUCApp/RUC.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ConsultaRUCApp
+{
+    public class RUC
+    {
+        public string ruc { get; set; }  
+        public string razon_social { get; set; }  
+        public string ciiu { get; set; }    
+        public string fecha_actividad { get; set; } 
+        public string contribuyente_condicion { get; set; } 
+        public string contribuyente_tipo { get; set; } 
+        public string contribuyente_estado { get; set; } 
+        public string nombre_comercial { get; set; } 
+        public string fecha_inscripcion { get; set; } 
+        public string domicilio_fiscal { get; set; } 
+        public string sistema_emision { get; set; } 
+        public string sistema_contabilidad { get; set; } 
+        public string actividad_exterior { get; set; }  
+        public string emision_electronica { get; set; } 
+        public string fecha_inscripcion_ple { get; set; }  
+        public string Oficio { get; set; }  
+        public string fecha_baja { get; set; } 
+        public string[] representante_legal { get; set; } 
+        public string[] empleados { get; set; } 
+        public string[] locales { get; set; } 
+    }
+}
